7d6f74b689c50acb61108b7bfcb56ad6faad51a3
sina majidi <sin.majidi@gmail.com>
2022-09-18 01:15:39 +0430

    this is some change on database

--- blog/temp_db_filler.py
+++ blog/temp_db_filler.py
@@ -5,17 +5,16 @@ db.create_all()
 
 #------------write data base---------------------
 # u = User(username='sina majidi', email='sin.majidi@gmail.com',password='sm1818')
-# p=Post(titile='me',content='its about me',author=u)
 # p=Post(title='me',content='its about me',author=u)
 # db.session.add(p)
 # db.session.commit()
 
 #---------------read data base--------------------
 print(User.query.all())
-print(User.query.first().posts)
+# print(User.query.first().posts)
 print(Post.query.all())
-print(Post.query.first().user_id)
-print(Post.query.first().author)
+# print(Post.query.first().user_id)
+# print(Post.query.first().author)
 # print(Post.query.filter_by(user_id='admin').first())
 # print(article.query.get(#primary key#))
 #------------------delete data-----------------------
